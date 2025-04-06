1fe1a3a61041a1901381f05371bb2a8a4ae73008
Mary_All <mariiayatsenko7@gmail.com>
2025-04-06 10:53:11 +0300

    add index file

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-    return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border) + "px";
+    return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
 } 
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
